0a3bd2dc9d37829bc532fe3d19765a3e67689c94
Daniyalpr <abdshahdaniyal@gmail.com>
2026-02-16 11:27:08 +0330

    feat: add spawn function.

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+import random
 import tkinter as tk
 root = tk.Tk()
 SIZE = 4
 TILE_SIZE = 100
 class Game():
     def __init__(self, root, nums:list=None):
+        """
+        nums: A 2d list conataining numbers in string. Note that "" means empty tile.
+        """
         self.nums = nums or [["" for _ in range(SIZE)] for _ in range(SIZE)]
         self.root = root
         self.frame = tk.Frame(root, bg="gray")
@@ -24,6 +28,18 @@ class Game():
                 label.grid(row=i, column=j, padx=5, pady=5)
                 row.append(label)
             self.tiles.append(row)
+
+        self.spawn()
+        self.spawn()
+    def spawn(self):
+        emp = []
+        for i in range(SIZE):
+            for j in range(SIZE):
+                if self.tiles[i][j].cget("text") == "":
+                    emp.append((i, j))
+        x, y = random.choice(emp)
+        self.tiles[x][y].config(text=2)
+
 
 game = Game(root)
 root.mainloop()
